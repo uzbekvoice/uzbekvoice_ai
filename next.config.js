e4f20fe2c29f9d3bfbebf6b9e3c702e12cc10c55
Javohir Hakimov <uzwebprogrammer@gmail.com>
2023-04-07 14:46:46 +0500

    old version some files

---
+++ next.config.js
@@ -0,0 +1,58 @@
+
+module.exports = {
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["uz-UZ", "en-US", "ru-RU"],
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "uz-UZ", 
+    localeDetection: false   
+  },
+
+  reactStrictMode: true,
+  swcMinify: true,
+
+
+  async redirects() {
+    return [
+      {
+        source: '/bosqich',
+        destination: '/bosqich/1',
+        permanent: false,
+      },
+      {
+        source: '/hakaton',
+        destination: '/hakaton/1',
+        permanent: false,
+      },
+      {
+        source: '/teams',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/guide',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
+
+  
+}
+
+// publicRuntimeConfig: {
+//   url: process.env.DIRECTUS_URL,
+// },
+// serverRuntimeConfig: {
+//   email: process.env.DIRECTUS_EMAIL,
+//   password: process.env.DIRECTUS_PASSWORD,
+//   // token: process.env.DIRECTUS_STATIC_TOKEN,
+// },
+
+
+
+
+
+
+
+
